291afafefac7416f577a6eed5fe34754e4620fba
@SergeyChernykh30 <sergey.chernykh.89@icloud.com>
2019-11-17 17:43:18 +0500

    added week 2 task 1 solution

---
+++ ps/week2/task1/chernykh/homeWork2Task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int enteredNumber;
+	int sum = 0;
+	int rangeEnd = 501;
+
+	cout << "Please enter a numner from 0 to 500\n";
+	cin >> enteredNumber;
+	do
+
+	{
+		sum += enteredNumber;
+		enteredNumber++;
+	} while (enteredNumber<rangeEnd);
+	cout << "Sum from your entered number to 500 = " << sum;
+}
