0a2c0e3eb53b5aece4f558fbcf155f522db833b9
VockresenskayaM <vmm459@gmail.com>
2019-11-23 19:52:24 +0500

    added week2 solutions

---
+++ ps/week2/task1/voskresenskaya/21.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	cout << "Enter the number i" << endl;
+	cin >> i;
+	long sum = 0;
+	if (i <= 500) {
+		for (i; i <= 500; i++)
+		{
+			sum += i;
+		}
+		cout << sum << endl;
+	}
+	else {
+		cout << "The number coult not be more 500 " << endl;
+	}
+	return 0;
+
+	
+}
+
+
---
+++ ps/week2/task6/voskresenskaya/26.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a=0;
+	float b;
+	cout << "b=" << endl;
+	cin >> b;
+	for (a=0; a<=b; a++)
+	{
+		cout << a << endl;
+	}
+	return 0;
+}
+
+
