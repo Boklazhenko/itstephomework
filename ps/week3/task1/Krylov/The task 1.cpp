64054c61f640275f78d5b32e36b87743ee017489
Paketa911 <paketathebest@bk.ru>
2019-11-28 22:47:26 +0500

    added week3 solutions

---
+++ ps/week3/task1/Krylov/The task 1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int result = 0;
+	int i1, i2, i3;
+	for (int i = 100; i <= 999; i++)
+	{
+		i1 = i / 100; //первая цифра
+		i2 = (i % 100) / 10; //вторая цифра
+		i3 = i % 10; //третья цифра
+		if (i1 == i2 || i2 == i3 || i3 == i1)
+			result++;
+
+	}
+	cout << "Результат = " << result << endl;
+}
---
+++ ps/week3/task5/Krylov/The task 5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	int a, b, result = 0;
+
+	cout << "Введите число = ";
+	cin >> a;
+
+	for (b = a; b > 0; b /= 10) {
+		result += b % 10;
+	}
+
+	if (result * result * result == a * a)
+		cout << "Ок";
+
+	else
+		cout << "Not ok";
+}
