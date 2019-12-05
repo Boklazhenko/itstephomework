6b4f8fc3e280b5ba8a53f1ede898752929531fcf
BadMaas <bad_m@bk.ru>
2019-12-05 12:19:27 +0500

    added week4

---
+++ ps/week4/task2/Shumilova/main2.cpp
@@ -0,0 +1,40 @@
+﻿//Задание 2. (неделя 4)
+// Написать программу, которая выводит на экран 
+//шахматную доску с заданным размером клеточки. 
+// ###---###--- 
+// ###---###--- 
+// ###---###--- 
+// ---###---###
+// ---###---###
+// ---###---###
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	int cells, size;
+	cout << "enter the number of cells ";
+	cin >> cells;
+	cout << "enter cell size ";
+	cin >> size;
+
+	for (int i = 1; i <= cells; ++i)
+	{
+		for (int j = 0; j < size; ++j)
+		{
+			for (int j = 0; j < cells; ++j)
+			{
+				for (int k = 0; k < size; ++k)
+				{
+					if ((i + j) % 2 == 1)
+					{
+						cout << "# ";
+					}
+					else cout << "- ";
+				}
+			}
+			cout << endl;
+		}
+	}
+}
---
+++ ps/week4/task6/Shumilova/main6.cpp
@@ -0,0 +1,36 @@
+﻿// Задание 6. (неделя 4)
+//Написать программу, копирующую последовательно 
+//элементы одного массива размером 10 элементов 
+//в 2 массива размером 5 элементов каждый.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	const int size = 10;
+	int big_array[size] = { 1,3,5,7,9,2,0,4,6,8 };
+	int small_array[5];
+	int little_array[5];
+	
+	cout << "Big array: "; 
+	for (int i = 0; i < size; ++i)
+	{
+		cout << big_array[i] << ", ";//выводим Big array: 1,3,5,7,9,
+	}
+	
+	cout << "\n\nSmall array: "; 
+	for (int i = 0; i < 5; ++i)
+	{
+		small_array[i]=big_array[i];
+		cout <<small_array[i]<<", ";//выводим Small array: 1,3,5,7,9,
+	}
+	cout << "\n\nLittle array: "; 
+	for (int i = 0; i < 5; ++i)
+	{
+		little_array[i] = big_array[i+5];
+		cout  << little_array[i] << ", ";//выводим Little array: 2,0,4,6,8,
+	}
+	cout << "\n\n";
+	
+}
+
