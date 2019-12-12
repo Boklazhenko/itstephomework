0dde6acde145dc98eab4aab9d028e1f4777300ff
VockresenskayaM <vmm459@gmail.com>
2019-12-12 21:53:00 +0500

    added week5 solutions

---
+++ ps/week5/task2/Voskresenskaya/task2.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	const int rou = 3;
+	const int col = 4;
+	int arr[rou][col];
+	int firstElement;
+	cout << "Enter the first element of the array" << endl;
+	cin >> firstElement;
+	for (int i = 0; i < rou; ++i)
+	{
+
+		for (int j = 0; j < col; ++j)
+		{
+			if (i == 0 && j == 0)
+			{
+				arr[i][j] = firstElement;
+				cout << arr[i][j] << " ";
+			}
+			else
+			{
+				firstElement = ++firstElement;
+				arr[i][j] = firstElement;
+				cout << arr[i][j] << " ";
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
