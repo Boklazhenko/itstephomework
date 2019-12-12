61dcc4005b39e67895bb3188c72ff84695016d14
TanyaBud <57768870+TanyaBud@users.noreply.github.com>
2019-12-12 23:15:02 +0500

    my home work week5

---
+++ ps/week5/task4/budrina/task_4.cpp
@@ -0,0 +1,49 @@
+﻿
+//В двумерном массиве целых чисел посчитать :
+//-Сумму всех элементов массива
+//-Среднее арифметическое всех элементов массива
+//-Минимальный элемент
+//-Максимальный элемент
+
+#include <iostream>
+#include<time.h>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+	srand(2);
+	const int size = 6;
+	int array[size][size];
+
+	for (int x = 0; x < size; ++x)
+	{
+		for (int y = 0; y < size; ++y)
+		{
+			array[x][y] = rand() % 100;
+		}
+	}
+	int sum = 0;
+	int min = array[0][0];
+	int max = array[0][0];
+	for (int x = 0; x < size; ++x)
+	{
+		for (int y = 0; y < size; ++y)
+		{
+			cout << array[x][y] << "\t";
+			sum += array[x][y];
+
+			if (array[x][y] < min)
+				min = array[x][y];
+
+			if (array[x][y] > max)
+				max = array[x][y];
+		}
+		cout << "\n" << endl;
+	}
+	cout << "Sum = " << sum << endl; 
+	float mean = (float)sum / (size * size);
+	cout << "Mean = " << mean << endl; 
+	cout << "Min = " << min << endl; 
+	cout << "Max = " << max << endl; 
+
+}
---
+++ ps/week5/task5/budrina/task_5.cpp
@@ -0,0 +1,52 @@
+﻿//В двумерном массиве целых чисел посчитать сумму элементов : в каждой строке; в каждом
+//столбце; одновременно по всем строкам и всем столбцам.Оформить следующим образом :
+//3  5  6  7  | 21
+//12 1  1  1  | 15
+//0  7  12 1  | 20
+//------------ -
+//15 13 19 9  | 56
+
+#include <iostream>
+#include<time.h> 
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+	srand(0);
+	const int size = 4;
+	int array[size][size];
+
+	for (int x = 0; x < size; ++x)
+	{
+		int sumRow = 0;
+		for (int y = 0; y < size; ++y)
+		{
+			array[x][y] = rand() % 100;
+			cout << array[x][y] << "\t";
+			sumRow += array[x][y];
+		}
+		cout << "| " << sumRow << "\n";
+		cout << "\n" << endl;
+	}
+
+	for (int i = 0; i <= size; ++i)
+	{
+		cout << "----\t";
+	}
+	cout << "\n" << endl;
+
+	int sumAll = 0;
+	for (int y = 0; y < size; ++y)
+	{
+		int sumCol = 0;
+		for (int x = 0; x < size; ++x)
+		{
+			sumCol += array[x][y];
+		}
+
+		cout << sumCol << "\t";
+		sumAll += sumCol;
+	}
+	cout << "| " << sumAll;
+	cout << "\n" << endl;
+}
