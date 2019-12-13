922df39ab1a5ed09a1f90d709a885cc8c42f5157
Kiryakov-S <exotic321@mail.ru>
2019-12-13 21:07:34 +0500

    home work week5

---
+++ ps/week5/task5/kiryakov/task5.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	const int m = 7, n = 5;
+	int ourMatrix[m][n] = {};
+	int rowSum[m] = {};
+	int columnSum[n] = {};
+	int totalSum = 0;
+
+	srand(time(NULL));
+	for (int rowNum = 0; rowNum < m; rowNum++)
+	{
+		for (int columnNum = 0; columnNum < n; columnNum++)
+		{
+			ourMatrix[rowNum][columnNum] = rand() % 10;
+			cout << setw(4) << ourMatrix[rowNum][columnNum] << "    ";
+			rowSum[rowNum] += ourMatrix[rowNum][columnNum];
+			columnSum[columnNum] += ourMatrix[rowNum][columnNum];
+
+		}
+		cout << " | " << rowSum[rowNum] << endl;
+	}
+	cout << "-------------------------------------" << endl;
+	for (int columnNum = 0; columnNum < n; columnNum++)
+	{
+		cout << setw(4) << columnSum[columnNum] << "    ";
+		totalSum += columnSum[columnNum];
+	}
+	cout << " | " << totalSum << endl;
+
+}
