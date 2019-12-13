3988513fbdc702039e189707c740d6beaf2879af
Paketa911 <paketathebest@bk.ru>
2019-12-13 16:56:14 +0500

    added week5

---
+++ ps/week5/task5/Krylov/The task 5.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	const int row = 2;
+	const int col = 5;
+	int arr[row][col];
+	int sumArr[row]{};
+	int sum = 0;
+	int sumAll = 0;
+
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+		{
+			arr[i][j] = rand() % 10 + 1;
+			cout << arr[i][j] << "\t";
+			sum += arr[i][j];
+			sumArr[i] += arr[i][j];
+			sumAll += arr[i][j];
+
+		}
+
+		cout << " |  " << sum << endl;
+	}
+	for (int i = 0; i < row; ++i)
+	{
+
+		cout << sumArr[i] << "\t";
+
+	}
+
+	cout << sumAll << "\t";
+
+	cout << endl;
+
+}
+
