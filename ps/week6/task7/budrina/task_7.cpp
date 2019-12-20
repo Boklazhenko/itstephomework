8cfa791de37d20f278081bd101076ba963a93f00
TanyaBud <57768870+TanyaBud@users.noreply.github.com>
2019-12-20 21:22:18 +0500

    my home work week6

---
+++ ps/week6/task7/budrina/task_7.cpp
@@ -0,0 +1,29 @@
+﻿// Написать функцию, определяющую количество положительных, отрицательных и нулевых элементов
+// передоваемого ей массива
+#include <iostream>
+using namespace std;
+void printArr(double arr[], int size)
+{
+	double positiveNum = 0;
+	double negativeNum = 0;
+	int zeroNum = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		if (arr[i] > 0) ++positiveNum;
+		else if (arr[i] < 0) ++negativeNum;
+		else  ++zeroNum;
+	}
+	cout << "\nPozitive number = " << positiveNum;
+	cout << "\nNegative number = " << negativeNum;
+	cout << "\nZero number = " << zeroNum<< endl;
+}
+int main()
+{
+	const int size = 10;
+	double arr[size] = { -2.2,4,-3.7,5,0,6,8,-9,0,1 };
+	for (int i = 0; i < size; ++i) {
+		cout << arr[i] << ", ";
+	}
+	printArr(arr, 10);
+	return 0;
+}
