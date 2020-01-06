8f825b50b35b5636944ada370c8e2daa2759737e
@SergeyChernykh30 <sergey.chernykh.89@icloud.com>
2020-01-06 21:48:11 +0500

    added week7.1 solutions

---
+++ ps/week7.1/task1/chernykh/homeWork7.1Task1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+void bubbleSort(int array[], int arraySize)
+{
+	bool swapped = false;
+
+	for (int i = 0; i < arraySize - 1; ++i)
+	{
+		swapped = false;
+		for (int j = 0; j < arraySize - i - 1; ++j)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				swapped = true;               //были ли перестановки
+			}
+		}
+		if (!swapped)
+		{
+			break;
+		}
+	}
+}
+
+void fillArray(int array[], int size)
+{
+	srand(time(0));
+	for (int i = 0; i < size; ++i)
+	{
+		array[i] = rand() % 100;
+	}
+}
+
+void printArray(int array[], int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		cout << array[i] << "\t";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	const int arraySize = 10;
+	int array[arraySize];
+
+	fillArray(array, arraySize);
+	printArray(array, arraySize);
+	bubbleSort(array, arraySize);
+	printArray(array, arraySize);
+}
---
+++ ps/week7.1/task4/chernykh/homeWork7.1Task4.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+void quickSortR(int* array, int arraySize)
+{
+	int i = 0, j = arraySize - 1;
+	int mid = array[arraySize / 2];
+
+	do
+	{
+		while (array[i] < mid)
+		{
+			++i;
+		}
+		while (array[j] > mid)
+		{
+			--j;
+		}
+		if (i <= j)
+		{
+			int temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+
+			++i;
+			--j;
+		}
+	} while (i <= j);
+
+	if (j > 0)
+	{
+		quickSortR(array, j + 1);
+	}
+	if (i < arraySize)
+	{
+		quickSortR(&array[i], arraySize - i);
+	}
+}
+
+void fillArray(int array[], int size)
+{
+	srand(time(0));
+	for (int i = 0; i < size; ++i)
+	{
+		array[i] = rand() % 100 + (-50);
+	}
+}
+
+void printArray(int array[], int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		cout << array[i] << "\t";
+	}
+}
+
+int main()
+{
+	const int arraySize = 9;
+	int array[arraySize];
+
+	fillArray(array, arraySize);
+	printArray(array, arraySize);
+
+	cout << endl;
+
+	double sumArrayElements = 0;
+	for (int i = 0; i < arraySize; ++i)
+	{
+		sumArrayElements += array[i];
+	}
+	double avgArray = sumArrayElements / (double)arraySize;
+
+	int thirdPartArray = arraySize / 3;
+	int x2ThirdPartArray = thirdPartArray * 2;
+
+	if (avgArray > 0)
+	{
+		quickSortR(array, x2ThirdPartArray);
+		for (int i = x2ThirdPartArray, j = arraySize - 1; i < j; ++i, --j)
+		{
+			int temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+	}
+	else
+	{
+		quickSortR(array, thirdPartArray);
+		for (int i = thirdPartArray, j = arraySize - 1; i < j; ++i, --j)
+		{
+			int temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+	}
+
+	printArray(array, arraySize);
+
+
+}
