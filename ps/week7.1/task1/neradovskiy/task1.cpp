d9029d9d2a08603bfd62b31a1f05ed9f87e64696
Neradovskiy <bopoh143@gmail.com>
2020-01-09 18:46:52 +0500

    added week7.1

---
+++ ps/week7.1/task1/neradovskiy/task1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+void bubbleSort(int arr[], int size) {
+	for (int i = 0; i < size; ++i)
+	{
+		for (int x = size - 1; x > i; --x)
+		{
+			if (arr[x - 1] > arr[x]) {
+				int temp = arr[x - 1];
+				arr[x - 1] = arr[x];
+				arr[x] = temp;
+			}
+		}
+	}
+
+}
+
+int main()
+{
+	srand(time(NULL));
+	const int size = 100;
+	int arr[size];
+	for (int i = 0; i < size; ++i) {
+		arr[i] = rand() % 100;
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+	bubbleSort(arr, size);
+	for (int i = 0; i < size; ++i)
+		std::cout << arr[i] << " ";
+}
+
---
+++ ps/week7.1/task4/neradovskiy/task4.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+void quickSortR(int arr[], int size);
+bool calcAverageGreterZero(int arr[], int size);
+void swith(int arr[], int first, int last);
+
+int main()
+{
+	const int size = 10;
+	int arr[size] = {-22, -23, -25, 7, 5, -48, -55, -60, -70, 30 };
+	//srand(time(NULL));
+	//for (int i = 0; i < size; ++i) {
+	//	arr[i] = rand() % 100;
+	//}
+	std::cout << "\nAfter sort: " << std::endl;
+	for (int i = 0; i < size; ++i)
+		std::cout << arr[i] << " ";
+
+	if (calcAverageGreterZero(arr, size)) {
+		int otherSize = (size / 3) * 2;
+		quickSortR(arr, otherSize);
+		swith(arr, otherSize, size - 1);
+	}
+	else {
+		int otherSize = (size / 3);
+		quickSortR(arr, otherSize);
+		swith(arr, otherSize, size - 1);
+
+
+	}
+	std::cout << "\nBefore sort:\n";
+	for (int i = 0; i < size; ++i)
+		std::cout << arr[i] << " ";
+}
+
+void quickSortR(int arr[], int size) {
+	int left = 0, right = size - 1;
+	int temp, centr;
+	centr = arr[size / 2];
+	do {
+		while (arr[left] < centr) {
+			left++;
+		}
+		while (arr[right] > centr)
+		{
+			right--;
+		}
+		if (left <= right) {
+			temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+			left++;
+			right--;
+		}
+	} while (left <= right);
+	if (right > 0) quickSortR(arr, right + 1);
+	if (size > left) quickSortR(arr + left, size - left);
+}
+bool calcAverageGreterZero(int arr[], int size) {
+	int sum = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		sum += arr[i];
+	}
+	return ((sum / size) > 0 ? true : false);
+}
+void swith(int arr[], int first, int last) {
+
+	do {
+		int temp;
+		temp = arr[first];
+		arr[first] = arr[last];
+		arr[last] = temp;
+		first++;
+		last--;
+
+	} while (first < last);
+}
