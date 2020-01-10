16bce95ebce5cc7427a8d8e9e15cfc44d7cbccdb
TanyaBud <57768870+TanyaBud@users.noreply.github.com>
2020-01-10 22:29:11 +0500

    add myhomework 7.1

---
+++ ps/week7.1/task3/budrina/task3.cpp
@@ -0,0 +1,61 @@
+﻿// Написать программу "успеваемость". Пользователь вводит 10 оценок студента.
+// Реализовать меню для пользоавателя. - вывод оценок (вывод содержимого массива)
+// - Пересдача экзамена (пользователь водит номер элемента массива и новую оценку)
+// - Выходит ли стипендия (выходит, если средний балл не ниже 10.7)
+#include <iostream> 
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int key;
+	const int size = 10;
+	int arr[size];
+	cout << "Проставьте 10 оценок от 8-12 баллов\n";
+	for (int i = 0; i < size; ++i)
+	{
+		cout << i + 1 << " оценка: ";
+		cin >> arr[i];
+	}
+	do {
+		cout << "\n\n Вывести все баллы: нажмите 1\n";
+		cout << " Пересдача экзамена: нажмите 2\n";
+		cout << " Выходит ли степендия: нажмите 3\n";
+		cout << " Выход из программы : нажмите 0\n";
+		cin >> key;
+		switch (key)
+		{
+		case 1:
+			for (int i = 0; i < size; ++i)
+				cout << arr[i] << "  ";
+			break;
+		case 2:
+			int repl;
+			cout << "Введите номер оценки для пересдачи:";
+			cin >> repl;
+			cout << "Старый бал " << arr[repl - 1] << endl;
+			cout << "Введите новый бал:";
+			cin >> arr[repl - 1];
+			break;
+		case 3:
+			double average_mark;
+			double sum = 0;
+			for (int i = 0; i < size; ++i) {
+				sum += arr[i];
+			}
+			average_mark = sum / size;
+
+			cout << "Средний бал успеваемости = " << average_mark << endl;
+
+			if (average_mark == 10.7 || average_mark > 10.7) {
+
+				cout << "Стипендия положена \n";
+			}
+			else {
+				cout << "Стипендия не положена \n";
+			}
+			break;
+		}
+	} 
+	while (key != 0);
+	return 0;
+}
---
+++ ps/week7.1/task4/budrina/task4.cpp
@@ -0,0 +1,61 @@
+﻿//Необходимо отсортировать первые две трети массива в порядке возрастания,
+//если среднее арифметическое всех элементов больше нуля;
+//иначе - лишь первую треть.
+//Остальную часть массива не сортировать, а расположить в обратном порядке.
+
+#include <iostream>
+#include <cstdlib> //for rand()
+#include <ctime> //for srand(time(NULL));
+
+using namespace std;
+void sortArray(int array[], int halfArray);
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	const int sz = 12;
+	int array[sz];
+	srand(time(NULL));
+
+	cout << "\n Выводим массив : ";
+	for (int i = 0; i < sz; i++)
+	{
+		array[i] = rand() % 101 - 50; // диапазон от -50 до 50
+
+		cout << array[i] << " ";
+	}
+	cout << "\n\n";
+
+	float averageArray = 0; //среднее арифметическое
+	for (int i = 0; i < sz; ++i) { //вычисление среднего арифметического массива
+		averageArray += array[i];
+		averageArray /= sz;
+	}
+	cout << " Average = " << averageArray << endl;
+	cout << "\n";
+
+	int halfArray; //часть массива
+	if (averageArray > 0)
+		halfArray = (sz / 3) * 2; // две трети массива
+	else halfArray = sz / 3; // одна треть массива
+
+	sortArray(array, halfArray); //сортировка массива
+	cout << " New sort array : ";
+	for (int i = 0; i < halfArray; ++i)
+		cout << array[i] << " ";       //вывод измененного массива
+	cout << "\n\n New end array : ";  
+	for (int i = sz - 1; i >= halfArray; --i)
+		cout << array[i] << " ";     //вывод оставшейся части в обратном порядке
+	cout << "\n\n";
+}
+void sortArray(int array[], int halfArray)
+{
+	for (int i = 0; i < halfArray; ++i)
+		for (int j = halfArray - 1; j > i; --j)
+			if (array[j - 1] > array[j])
+			{
+				int temp = array[j - 1];
+				array[j - 1] = array[j];
+				array[j] = temp;
+			}
+}
