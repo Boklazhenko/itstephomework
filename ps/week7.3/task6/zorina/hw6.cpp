80bc586fab565be26d81db41fd9791fd716986fd
annazorina <57866564+annazorina@users.noreply.github.com>
2020-01-10 13:45:13 +0500

    added solution week 7.3

---
+++ ps/week7.3/task6/zorina/hw6.cpp
@@ -0,0 +1,19 @@
+﻿// Используя указатели и оператор разыменования, обменять местами значения двух переменных.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cout << "Plese, enter the first\n";
+	cin >> x;
+	cout << "Please, enter the second\n";
+	cin >> y;
+	int* pX = &x, * pY = &y;
+	int temp = *pX;
+	*pX = *pY;
+	*pY = temp;
+	cout << "Now the first number = " << *pX << ", the second number = " << *pY;
+}
