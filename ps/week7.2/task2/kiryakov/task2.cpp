ab6c0a03e460cacf8423d13066a36a8ae2dc4a73
Kiryakov-S <exotic321@mail.ru>
2020-01-15 21:31:27 +0500

    home work week7.2

---
+++ ps/week7.2/task2/kiryakov/task2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <time.h>
+using namespace std;
+void main()
+{
+	int random, user, rmas[4], umas[4], c = 0, bulls = 0, cows = 0;
+	srand(time(0));
+	random = rand() % (9999 - 1000 + 1) + 1000;
+	for (int i = 3; i >= 0; --i)
+	{
+		rmas[i] = random % 10;
+		random /= 10;
+	}
+
+	while (cows != 4)
+	{
+		bulls = 0, cows = 0;
+		cout << "Enter a 4-digit number or 0 (zero) to exit: ";
+		cin >> user;
+		if (user == 0)
+		{
+			break;
+		}
+		++c;     
+		for (int i = 3; i >= 0; --i)
+		{
+			umas[i] = user % 10;
+			user /= 10;
+		}
+
+		for (int i = 0; i < 4; i++)
+		{
+			if (umas[i] == rmas[0] || umas[i] == rmas[1] || umas[i] == rmas[2] || umas[i] == rmas[3])
+			{
+				++bulls;
+			}
+			if (umas[i] == rmas[i])
+			{
+				++cows;
+			}
+		}
+		cout << "Bulls: " << bulls << "\tCows: " << cows << "\n";
+	}
+
+	if (cows == 4)
+	{
+		cout << "\nYou guessed  " << c << "try!" << "\n";
+	}
+	else
+	{
+		cout << ":(\n";
+	}
+
+	
+}
---
+++ ps/week7.2/task6/kiryakov/task6.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int sum(int a, int b)
+{
+	if (a - b == 1)
+		return 0;
+	return b + sum(a, b - 1);
+}
+int main()
+{
+	int a;
+	cout << "\nEnter a: ";
+	cin >> a;
+
+	int b;
+	cout << "\nEnter b: ";
+	cin >> b;
+	if (a == b || a > b)
+		return 0;
+
+	cout << "\nThe sum of all numbers, between " << a << " and "
+		<< b << " is " << sum(a, b) << "\n";
+	return 0;
+}
