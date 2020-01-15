738bd2cbc837c6960510c287142b691aef9a5092
Kiryakov-S <exotic321@mail.ru>
2020-01-15 21:02:06 +0500

    home work week7.1

---
+++ ps/week7.1/task2/kiryakov/task2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+#include <time.h>
+
+void main()
+
+{
+    const int M = 6;
+
+	srand(time(0));
+
+	int a[M], j = 0, tmp;
+
+	for (int i = 0; i < M; i++) {
+
+		a[i] = rand() % 10;
+
+	}
+	for (int i = 0; i < M; i++) {
+		cout << a[i] << " ";
+	}
+	cout << "\n";
+	for (int i = 0; i < M - 1; i++) {
+
+		tmp = a[i + 1];
+
+		j = i;
+
+		while (j >= 0 && a[j] > tmp) {
+
+			a[j + 1] = a[j];
+
+			j--;
+
+		}
+
+		a[j + 1] = tmp;
+
+	}
+	cout << "\n";
+	for (int i = 0; i < M; i++)
+		cout << a[i] << "\t";
+	cout << "\n";
+
+}
