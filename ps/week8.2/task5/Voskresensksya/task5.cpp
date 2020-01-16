10df2ca4ea45a26032a7122ed49331dacc46b452
VockresenskayaM <vmm459@gmail.com>
2020-01-16 22:35:31 +0500

    added week8.2

---
+++ ps/week8.2/task5/Voskresensksya/task5.cpp
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит строку символов и искомый символ, посчитать
+//сколько раз он встречается в строке.
+
+#include <iostream>
+using namespace std;
+
+ int countСhar(char *str, char character)
+{
+	int i = 0, count = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == character)
+		{
+			++count;
+		}
+			++i;
+	}
+	return count;
+}
+
+int main()
+{
+	const int size = 100;
+	char arr[size];
+	char a;
+	cout << "Enter any string but no more then " << size << "symbols" << endl;
+	cin >> arr;
+	cout << "Enter the character you want to count" << endl;
+	cin >> a;
+	cout << countСhar(arr, a);
+	return 0;
+
+}
+
+
---
+++ ps/week8.2/task6/Voskresenskaya/task6.cpp
@@ -0,0 +1,41 @@
+﻿// Пользователь вводит строку. Определить количество букв, 
+//количество цифр и количество остальных символов, присутствующих в строке.
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string str;
+	cout << "Enter offers" << endl;
+	getline(cin, str);
+	int countAlf = 0;
+	int countDig = 0;
+	int countSpac = 0;
+	int countAlnum = 0;
+	for (int i = 0; i < str.size(); ++i)
+	{
+		if (isalpha(str[i]))
+		{
+			++countAlf;
+		}
+		else if  (isdigit(str[i]))
+		{
+			++countDig;
+		}
+		else if (isspace(str[i]))
+		{
+			++countSpac;
+		}
+		else
+		{
+			++countAlnum;
+		}
+	}
+	cout << "Number of letters in a sentence= " << countAlf<< endl;
+	cout << "Number of digits in a sentence= " << countDig << endl;
+	cout << "Number of supply gaps = " << countSpac << endl;
+	cout << "Number of characters in a sentence = " << countAlnum << endl;
+	return 0;
+}
+
