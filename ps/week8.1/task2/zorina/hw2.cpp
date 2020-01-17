ed7fbc7cdb79659f7a0092b437aa8f2d514841f1
annazorina <57866564+annazorina@users.noreply.github.com>
2020-01-17 12:34:33 +0500

    Solutions week 8.1

---
+++ ps/week8.1/task2/zorina/hw2.cpp
@@ -0,0 +1,127 @@
+﻿// Даны два массива: А[M] и B[N] (M и N вводятся с клавиатуры). Необходимо создать третий массив минимально возможного размера,
+//в котором нужно собрать элементы массивов A и B, которые не являются общими для них, без повторений.
+//
+
+#include <iostream>
+using namespace std;
+
+void fillArray(int* arr, int size) {
+	for (int i = 0; i < size; ++i)
+		arr[i] = rand() % 30;
+}
+
+void printArray(int* arr, int size) {
+	for (int i = 0; i < size; ++i)
+		cout << *(arr + i) << "\t";
+	cout << endl;
+}
+
+
+int main()
+{
+
+	int size = 0, size1, size2;
+
+	cout << "Input the size of the first array\n";
+	cin >> size1;
+	int* arr1 = new int[size1];
+	fillArray(arr1, size1);
+
+	cout << "Input the size of the second array\n";
+	cin >> size2;
+	int* arr2 = new int[size2];
+	fillArray(arr2, size2);
+
+
+	printArray(arr1, size1);
+	cout << endl;
+	printArray(arr2, size2);
+	cout << endl;
+
+	//вычисляем размер нового массива
+
+
+	for (int i = 0; i < size1; ++i) { 
+
+		bool ok = true;
+
+		int counter = 0;
+		for (int x = i + 1; x < size1; ++x) 
+			if (arr1[x] == arr1[i])
+				ok = false;
+
+		if (ok) {
+			for (int j = 0; j < size2; ++j) {
+				if (arr1[i] != arr2[j])       
+					++counter;
+
+			}
+			if (counter == size2)
+				++size;           
+		}
+	}
+	for (int i = 0; i < size2; ++i) { 
+
+		bool ok = true;
+		int counter = 0;
+		for (int x = i + 1; x < size2; ++x) 
+			if (arr2[x] == arr2[i])
+				ok = false;
+
+		if (ok) {
+			for (int j = 0; j < size1; ++j) { 
+				if (arr2[i] != arr1[j])       
+					++counter;
+
+			}
+			if (counter == size1)
+				++size;           
+		}
+	}
+
+	int* arr = new int[size]; 
+	int index = 0;
+	for (int i = 0; i < size1; ++i) { 
+		bool ok = true;
+		int counter = 0;
+
+		for (int x = i + 1; x < size1; ++x) 
+			if (arr1[x] == arr1[i])
+				ok = false;
+
+		if (ok) {
+			for (int j = 0; j < size2; ++j) 
+				if (arr1[i] != arr2[j])  
+					++counter;
+
+			if (counter == size2) {
+				arr[index] = arr1[i];
+				++index;
+			}
+		}
+	}
+	for (int i = 0; i < size2; ++i) { 
+		bool ok = true;
+		int counter = 0;
+
+		for (int x = i + 1; x < size2; ++x) 
+			if (arr2[x] == arr2[i]) ok = false;
+
+		if (ok) {
+			for (int j = 0; j < size1; ++j) 
+				if (arr2[i] != arr1[j]) 
+					++counter;
+
+			if (counter == size1) {
+				arr[index] = arr2[i];
+				++(index);
+			}
+		}
+	}
+
+	printArray(arr, size);
+
+	delete[]arr1;
+	delete[]arr2;
+	delete[]arr;
+}
---
+++ ps/week8.1/task5/zorina/hw5.cpp
@@ -0,0 +1,81 @@
+﻿// Дан массив: А[M] (M вводится с клавиатуры). Необходимо удалить из массива четные или нечетные значения. Пользователь вводит данные в массив,
+//а также с помощью меню решает, что нужно удалить.
+//
+
+#include <iostream>
+using namespace std;
+enum whatToDo { Exit, RemoveEvenNum, RemoveOddNum };
+
+
+void printArray(int* arr, int size) {
+	for (int i = 0; i < size; ++i)
+		cout << *(arr + i) << "\t";
+	cout << endl;
+}
+
+int main()
+{
+	int size, sizeNew = 0, whatToDo;
+
+	cout << "Input size of the array\n";
+	cin >> size;
+	int* arr = new int[size];
+	for (int i = 0; i < size; ++i) {
+		cout << "Input data\n";
+		cin >> arr[i];
+	}
+	printArray(arr, size);
+
+
+	cout << "Input 1, if you want to remove all even numbers\n";
+	cout << "Input 2, if you want to remove all odd numbers\n";
+	cout << "Input 0, if you want to exit the programm\n";
+	cin >> whatToDo;
+	switch (whatToDo)
+	{
+	case Exit:
+		return 0;
+		break;
+	case RemoveEvenNum:
+	{
+		for (int i = 0; i < size; ++i) {
+		if (arr[i] % 2 == 1)
+			++sizeNew;
+	}
+	int* arrNew = new int[sizeNew];
+	int j = 0;
+	for (int i = 0; i < size; ++i) {
+		if (arr[i] % 2 == 1) {
+			arrNew[j] = arr[i];
+			++j;
+		}
+	}
+	printArray(arrNew, sizeNew);
+	delete[] arrNew;
+	break;
+	}
+	case RemoveOddNum:
+	{
+		for (int i = 0; i < size; ++i) {
+		if (arr[i] % 2 == 0)
+			++sizeNew;
+	}
+	int* arrNew = new int[sizeNew];
+	int j = 0;
+	for (int i = 0; i < size; ++i) {
+		if (arr[i] % 2 == 0) {
+			arrNew[j] = arr[i];
+			++j;
+		}
+	}
+	printArray(arrNew, sizeNew);
+	delete[] arrNew;
+	break; 
+	}
+	default:
+		cout << "Wrong input, try 0 to 2\n";
+		break;
+	}
+
+	delete[]arr;
+}
