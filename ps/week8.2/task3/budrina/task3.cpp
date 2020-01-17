0741252570089c13616c5153b4336c9f7e9c2ff5
TanyaBud <57768870+TanyaBud@users.noreply.github.com>
2020-01-17 23:49:41 +0500

    my homework 8.2

---
+++ ps/week8.2/task3/budrina/task3.cpp
@@ -0,0 +1,36 @@
+﻿// Написать функцию, которая вставляет в строку в указанную позицию заданный символ.
+
+#include <iostream>
+#include<iterator>
+using namespace std;
+
+void Insert(char* s, int len, int k, char ch)
+{
+    for (int i = len; i > k - 2; i--)
+        s[i + 1] = s[i];
+    s[k - 1] = ch;
+}
+
+int main()
+{
+    int k, len = 0;
+    char s[256], ch;
+    cout << "String:\n";
+    cin.getline(s, 256);
+    cout << "Symbol:\n";
+    cin >> ch;
+    cout << "Position:\n";
+    cin >> k;
+
+    while (s[len])
+    {
+        len++;
+    }
+
+    Insert(s, len, k, ch);
+    cout << s << "\n";
+    return 0;
+}
+
+
+    
---
+++ ps/week8.2/task4/budrina/task4.cpp
@@ -0,0 +1,23 @@
+﻿// Написать программу, которая заменяет все символы точки "." в строке, введенной пользователем,
+// на символы восклицательного знака"!"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout << "Enter string: ";
+    getline(cin, str);
+
+    for (int i = 0; i < str.size(); ++i)
+    {
+        if (str[i] == '.') {
+            str[i] = '!';
+            continue;
+        }
+    }
+    cout << str << "\n";
+}
+
