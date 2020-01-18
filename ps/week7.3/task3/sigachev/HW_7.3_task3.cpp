f8533788504a30d2637ef4b3f7d53b01e4b377ef
SigachevSergey <locia@bk.ru>
2020-01-18 13:13:28 +0500

    sigachev home work week7.3

---
+++ ps/week7.3/task3/sigachev/HW_7.3_task3.cpp
@@ -0,0 +1,19 @@
+﻿// Используя два указателя на массивы целых чисел, скопировать один массив в другой так, чтобы во втором массиве элементы 
+// находились в обратном порядке.
+// Использовать в программе арифметику указателей для продвижения по массиву, а также оператор разыменования.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int size = 10;
+    int arr[size] = { 1,4,7,8,5,8,2,4,0,3 };
+    int inverseArr[size] = { 0 };
+    int* pArr = arr;
+    int* pInverseArr = inverseArr;
+    for (int i = 0; i < size; ++i) {
+        *(pInverseArr + i) = *(pArr + ((size - 1) - i));
+        cout << *(pInverseArr + i) << " ";
+    }
+}
---
+++ ps/week7.3/task5/sigachev/HW_7.3_task5.cpp
@@ -0,0 +1,18 @@
+﻿// Используя указатели и оператор разыменования, определить знак числа, введённого с клавиатуры.
+#include <iostream>
+using namespace std;
+
+void signNumber(int* pA) {
+    cout << "Sign of number " << (*pA > 0 ? "positive." : "negative.");
+}
+int main()
+{
+    int a;
+    cout << "Enter number: ";
+    cin >> a;
+    if (a == 0) {
+        cout << "your number zero!";
+        return 0;
+    }
+    signNumber(&a);
+}
