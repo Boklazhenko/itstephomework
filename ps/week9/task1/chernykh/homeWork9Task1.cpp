504abab03897c420ae086779dd692fb8106880df
@SergeyChernykh30 <sergey.chernykh.89@icloud.com>
2020-01-20 22:10:19 +0500

    added week9 solution

---
+++ ps/week9/task1/chernykh/homeWork9Task1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	FILE* pFile;
+	errno_t err;
+
+	err = fopen_s(&pFile, "TestFile.txt", "a+");
+
+	for (int i = 1; i < argc; ++i)
+	{
+		fprintf(pFile, "%s\n", argv[i]);
+	}
+
+	fclose(pFile);
+
+	if (argc == 1)
+	{
+		err = fopen_s(&pFile, "TestFile.txt", "r");
+		char buff[255];
+
+		while (fscanf_s(pFile, "%s\n", buff, _countof(buff)) != -1)
+		{
+			cout << buff << endl;
+		}
+
+		fclose(pFile);
+	}
+
+	system("pause");
+
+	return 0;
+}
