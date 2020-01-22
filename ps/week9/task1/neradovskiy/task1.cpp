67ab5c72835d961014737f5e1ccc3a24b161b098
Neradovskiy <bopoh143@gmail.com>
2020-01-22 18:26:44 +0500

    added week9 homework

---
+++ ps/week9/task1/neradovskiy/task1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	FILE* pFile;
+	errno_t err;
+	const int size = 256;
+	err = fopen_s(&pFile, "TestText.txt", "a+");
+	if (err)
+		printf_s("File not opened");
+
+	for (int i = 1; i < argc; ++i)
+	{
+		fprintf(pFile, "%s\n", argv[i]);
+	}
+	fclose(pFile);
+	if (argc == 1)
+	{
+		err = fopen_s(&pFile, "TestText.txt", "r");
+		char buffer[size];
+		while (fscanf_s(pFile, "%s\n", buffer, _countof(buffer)) != -1)
+		std::cout << buffer << std::endl;
+
+		fclose(pFile);
+	}
+}
