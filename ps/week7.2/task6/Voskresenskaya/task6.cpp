68f37b03d22e0064d011e1c06bb9ad76f6fb7cb9
VockresenskayaM <vmm459@gmail.com>
2020-01-23 18:20:14 +0500

    added week7.2 without 2 fnd 3 tasks

---
+++ ps/week7.2/task6/Voskresenskaya/task6.cpp
@@ -0,0 +1,35 @@
+﻿//Написать рекурсивную функцию, которая вычисляет сумму 
+//всех чисел в диапазоне от a до b.Пользователь вводит a и b.
+//Проиллюстрируйте работу функции примером.
+#include <iostream>
+using namespace std;
+int findSum(int a, int b, int sum = 0)
+{
+	if (a == b + 1)
+	{
+		cout << sum << endl;
+		return 0;
+	}
+	sum += a;
+	++a;
+	return findSum(a, b, sum);
+}
+
+int main()
+{
+	int a, b;
+	cout << "Enter two numbers first smaller then larger";
+	cin >> a;
+	cin >> b;
+	if (a < b)
+	{
+		findSum(a, b);
+	}
+	else
+	{
+		cout << "error";
+	}
+	return 0;
+}
+
+
---
+++ ps/week7.2/task7/Voskresenskaya/task7.cpp
@@ -0,0 +1,67 @@
+﻿//Напишите рекурсивную функцию, которая принимает одномерный массив из 100 целых чисел, 
+//заполненных случайным образом, и находит позицию, с которой начинается последовательность
+//из 10 чисел, сумма которых минимальна.
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int findMinTenSun(int arr[], int arr2[], int N = 0, int Sum = 0)
+{
+	if ((100 - N) == 10)
+	{
+		int minSum = 2147483646;
+		for (int i = 0; i < N; ++i)
+		{
+			if (arr2[i] < minSum)
+			{
+				minSum = arr2[i];
+			}
+		}
+		int M = 0;
+		for (int j = 0; j < 90; ++j)
+		{
+			int sum10Arr = 0;
+			for (int i = M; i < M + 10; ++i)
+			{
+				sum10Arr += arr[i];
+			}
+			if (sum10Arr == minSum)
+			{
+				cout << "index minSum= " << M << endl;
+				break;
+			}
+			++M;
+		}
+
+		return 0;
+	}
+	for (int i = N; i < N + 10; ++i)
+	{
+		Sum += arr[i];
+	}
+	arr2[N] = Sum;
+	++N;
+	return findMinTenSun(arr, arr2, N);
+}
+int main()
+{
+	srand(time(NULL));
+	int const size = 100;
+	int const size2 = 90;
+	int arr[size];
+	int arr2[size2];
+
+	for (int i = 0; i < size; ++i)
+	{
+		arr[i] = rand() % 100;
+	}
+	for (int i = 0; i < size; ++i)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << findMinTenSun(arr, arr2);
+	return 0;
+}
+
+
