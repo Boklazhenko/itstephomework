8067520977725ee5ee6b10d1824ca6d97d6985aa
TanyaBud <57768870+TanyaBud@users.noreply.github.com>
2020-01-24 23:10:07 +0500

    my homework week9

---
+++ ps/week9/task1/budrina/task1.cpp
@@ -0,0 +1,38 @@
+﻿//создать приложение, принимающую через командную строку аргументы.
+//
+//если аргументы есть - то записать их в файл
+//1 аргумент в 1 строке
+//(дописать, если файл существует)
+//если аргументов нет - вывести все записи в консоль
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char** argv) {
+	FILE* f;
+	
+	f = fopen("test.txt", "a+");
+
+	for (int i = 1; i < argc; ++i) {
+		fprintf(f, "%s\n", argv[i]);
+	}
+	fclose(f);
+	if (argc == 1)
+	{
+
+		f = fopen("test.txt", "r+");
+		char buff[255];
+		int age;
+		while (fscanf(f, "%s%d\n", buff, &age) != -1) {
+
+			cerr << buff << " " << age << endl;
+		};
+		fclose(f);
+
+	}
+	return 0;
+}
