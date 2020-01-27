9c79ad92cdaee9464168bf514999ebda33d3e237
yuriy pyankov <nail9292@list.ru>
2020-01-27 20:17:16 +0300

    added week9 solutions

---
+++ ps/week9/task1/pyankov/Home work 9.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS // Без этого ругается на небезопасную функцию fopen()
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	FILE* test;
+	test = fopen("test.txt", "a+");
+
+	for (int i = 1; i < argc; ++i) {
+		fprintf(test, "%s\n", argv[i]);
+	}
+	fclose(test);
+	
+	if (argc == 1)
+	{
+		test = fopen("test.txt", "r");
+		char arguments[100];
+		while (fscanf(test, "%s\n", arguments) != -1) // Не понимаю это сравнение и как меняется значение
+		{
+			cout << arguments << endl;
+		}
+		fclose(test);
+	}
+	return 0;
+}
+
