9b8bdd5473a5f349bdb012a29b4713db62e7c603
SigachevSergey <locia@bk.ru>
2020-02-01 04:19:12 +0500

    sigachev week 9

---
+++ ps/week9/task1/sigachev/HW_9_task1.cpp
@@ -0,0 +1,37 @@
+﻿//создать приложение, принимающую через командную строку аргументы.
+//если аргументы есть - то записать их в файл
+//1 аргумент в 1 строке
+//(дописать, если файл существует)
+//если аргументов нет - вывести все записи в консоль
+//п.с.самый первый аргумент, всегда присутствующий, имя исполняемой программы, учитывать не нужно
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	setlocale(LC_ALL, "");		// чтобы кирилица отображалась
+	
+	FILE* test;
+	fopen_s(&test, "test.txt", "a+");
+	
+	for (int i = 1; i < argc; ++i) {
+		fprintf(test, "%s\n", argv[i]);
+	}
+	fclose(test);
+
+	if (argc == 1){
+
+		fopen_s(&test, "test.txt", "r");
+		char buff[255];
+		while (fscanf_s(test, "%s\n", buff, _countof(buff)) != -1){
+
+			cout << buff << std::endl;
+		}
+		fclose(test);
+	}
+
+
+	system("pause");
+	return 0;
+}
