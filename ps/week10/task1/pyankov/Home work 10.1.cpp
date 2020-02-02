4d6327913e0e21781253beedd35ae6cfaabfc2c3
yuriy pyankov <nail9292@list.ru>
2020-02-02 21:40:57 +0300

    added week10 solutions

---
+++ ps/week10/task1/pyankov/Home work 10.1.cpp
@@ -0,0 +1,48 @@
+﻿// Понял как работать со сруктурами, но не понял как работать с комплексными числами.
+
+#include <iostream>
+using namespace std;
+
+struct ComplexNumber
+{
+	int realPart;
+	int imaginaryPart;
+};
+
+void printSumComplexNumber(const ComplexNumber& one, const ComplexNumber& two)
+{
+
+	int sum1 = (one.imaginaryPart + two.imaginaryPart);
+	int sum2 = (one.realPart + one.realPart);
+	cout << "Sum = " << sum2 << "+" << sum1 << "i" << endl;
+}
+
+void printSubtraction(const ComplexNumber& one, const ComplexNumber& two)
+{
+
+	int sub1 = (one.imaginaryPart - two.imaginaryPart);
+	int sub2 = (one.realPart - two.realPart);
+	cout << "Subtraction = " << sub2 << "+" << sub1 << "i" << endl;
+}
+
+void printMultiplication(const ComplexNumber& one, const ComplexNumber& two)
+{
+	int multStepOne = (one.realPart * two.realPart) + (one.imaginaryPart * two.realPart)
+		+ (one.realPart * two.imaginaryPart) + (one.imaginaryPart * two.imaginaryPart);
+	cout << "Multiplication = " << multStepOne << endl;
+
+}
+
+int main()
+{
+
+	ComplexNumber num1 = { 2,4 };
+	ComplexNumber num2 = { 2,4 };
+
+
+	printSumComplexNumber(num1, num2);
+	printSubtraction(num1, num2);
+	printMultiplication(num1, num2 );
+
+
+}
---
+++ ps/week10/task2/pyankov/Home work 10.2.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct Car
+{
+	string carBrand;
+	string carModel;
+	int length;
+	int clearance;
+	float engineVolume;
+	int enginePower;
+	int diameterWheels;
+	string color;
+	string transmission;
+	
+};
+
+
+Car creatNewCar(const string& carBrand, const string& carModel, const int& length, const int& clearance, const float& engineVolume,
+	const int& enginPower, const int& diameterWheels, const string& color, const string& transmission)
+{
+	Car newCar = { carBrand, carModel, length, clearance, engineVolume, enginPower, diameterWheels, color, transmission };
+
+	return newCar;
+}
+
+void showAllParameters(const Car& p)
+
+{
+	cout << "Car Brand: " << p.carBrand << endl;
+	cout << "Car Model: " << p.carModel << endl;
+	cout << "length: " << p.length << " mm" << endl;
+	cout << "clearance: " << p.clearance << " mm" << endl;
+	cout << "engineVolume: " << p.engineVolume << endl;
+	cout << "enginePower: " << p.enginePower << " l.s." << endl;
+	cout << "diameterWheels: " << p.diameterWheels << endl;
+	cout << "color: " << p.color << endl;
+	cout << "transmission: " << p.transmission << endl;
+	cout << endl;
+}
+
+
+
+void searchParameter(const Car& p, string par)
+{
+
+	if (par == "length")
+		cout << p.length;
+	else if (par == "clearance")
+		cout << p.clearance;
+	else if (par == "Volume")
+		cout << p.engineVolume;
+	else if (par == "Power")
+		cout << p.enginePower;
+	else if (par == "Wheels")
+		cout << p.diameterWheels;
+	else if (par == "Color")
+		cout << p.color;
+	else if (par == "Trans")
+		cout << p.transmission;
+	else
+		cout << "No ";
+
+
+}
+
+int main()
+{
+	string par;
+
+	Car BMW = { "BMW","525", 4422, 180, 2.0 , 170, 17, "Black", "manual" };
+	   
+	Car Audi = creatNewCar("Audi","A6",4480, 160, 3.5, 200, 18, "White", "auto");
+
+	showAllParameters(BMW);
+	showAllParameters(Audi);
+
+	cout << "Input parametr" << endl;
+	cin >> par;
+	
+	searchParameter(Audi,par);
+	
+
+
+}
+
