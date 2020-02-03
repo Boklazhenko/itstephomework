56a53a9e0db5ab67a2726b71a2f7f56f325903e9
VockresenskayaM <vmm459@gmail.com>
2020-02-03 21:17:51 +0500

    added week10 soulution

---
+++ ps/week10/task1/Voskresenskaya/task1.cpp
@@ -0,0 +1,61 @@
+﻿// Создайте структуру, описывающую комплексное число. Реализуйте арифметические операции с комплексными числами:
+// сумму, разность, умножение, деление.
+
+
+#include <iostream>
+using namespace std;
+struct ComplexNum {
+	float Re;
+	float Im;
+};
+ComplexNum FindSumComplexNum(ComplexNum &p, ComplexNum &z)
+{
+	ComplexNum Sum;
+	Sum.Re = (p.Re + z.Re);
+	Sum.Im = (p.Im + z.Im);
+	cout << "ComplexNumSum= " << Sum.Re << "+" << Sum.Im << "i" << endl;
+	return Sum;
+}
+ComplexNum FindDiffComplexNum(ComplexNum &p, ComplexNum &z)
+{
+	ComplexNum Diff;
+	Diff.Re = (p.Re - z.Re);
+	Diff.Im = (p.Im - z.Im);
+	if (Diff.Im >= 0)
+		cout << "ComplexNumDiff= " << Diff.Re << "+" << Diff.Im << "i" << endl;
+	else
+		cout << "ComplexNumDiff= " << Diff.Re << Diff.Im << "i" << endl;
+	return Diff;
+}
+ComplexNum FindMultComplexNum(ComplexNum &p, ComplexNum &z)
+{
+	ComplexNum Mult;
+	Mult.Re = (p.Re * z.Re - p.Im * z.Im);
+	Mult.Im = (p.Im * z.Re + p.Re * z.Im);
+	if (Mult.Im >= 0)
+		cout << "ComplexNumMult= " << Mult.Re << "+" << Mult.Im << "i" << endl;
+	else
+		cout << "ComplexNumMult= " << Mult.Re << Mult.Im << "i" << endl;
+	return Mult;
+}
+ComplexNum FindDivComplexNum(ComplexNum &p, ComplexNum &z)
+{
+	ComplexNum Div;
+	Div.Re = (p.Re*z.Re+p.Im*z.Im)/(z.Re*z.Re+z.Im*z.Im);
+	Div.Im = (z.Re*p.Im-p.Re*z.Im)/ (z.Re * z.Re + z.Im * z.Im);
+	if (Div.Im >= 0)
+		cout << "ComplexNumDiv= " << Div.Re << "+" << Div.Im << "i" << endl;
+	else
+		cout << "ComplexNumDiv= " << Div.Re << Div.Im << "i" << endl;
+	return Div;
+}
+int main()
+{
+	ComplexNum num1 = { 2,4 };
+	ComplexNum num2 = { 7,9 };
+	FindSumComplexNum(num1, num2);
+	FindDiffComplexNum(num1, num2);
+	FindMultComplexNum(num1, num2);
+	FindDivComplexNum(num1, num2);
+	return 0;
+}
---
+++ ps/week10/task5/Voskresenskaya/task5.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+#include <bitset>
+using namespace std;
+enum Option
+{
+	Bloced =1, // 0000 0001
+	Bronz=2, //0000 0010
+	Silver =4, // 0000 0100
+	Debtor =16, // 0001 0000
+	Gold =8 // 0000 1000
+};
+struct Client {
+	char Name[256];
+	uint8_t Age;
+	uint8_t Options;
+
+};
+
+void print(const Client& client)
+{
+	cout << client.Name << ":" << static_cast<uint16_t>(client.Age) << ": " << bitset<sizeof(client.Options) * 8>(client.Options) << endl;
+}
+void unsetOpsions( Client  &client, uint8_t options)
+{
+	
+	client.Options &= ~options;	
+}
+int main()
+{
+	Client client{
+		"Aleksandr",
+		31,
+		Gold|Bloced|Debtor
+	};
+	unsetOpsions(client, Bloced | Debtor);
+	print(client);
+	return 0;
+}
+
+
