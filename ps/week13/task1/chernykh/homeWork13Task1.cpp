2810f3415947914d57a6ce3a288e3596f9962973
@SergeyChernykh30 <sergey.chernykh.89@icloud.com>
2020-02-24 21:38:36 +0500

    added week13 solutions

---
+++ ps/week13/task1/chernykh/homeWork13Task1.cpp
@@ -0,0 +1,67 @@
+﻿//#define INTEGER
+//#define DOUBLE
+#define CHAR
+#include <iostream>
+#include "function.h"
+
+using namespace std;
+
+int main()
+{
+	const int SIZE = 20;
+	
+#ifdef INTEGER
+
+	int arrayInt[SIZE];
+
+	cout << "Fill and print array with Int's:" << endl;
+	fill(arrayInt, SIZE);
+	print(arrayInt, SIZE);
+	cout << "\nMin element = " << searchMin(arrayInt, SIZE) << endl;
+	cout << "Max element = " << searchMax(arrayInt, SIZE) << "\n\n";
+	cout << "Print array with Int's after sorting:" << endl;
+	sort(arrayInt, SIZE);
+	print(arrayInt, SIZE);
+	editEl(arrayInt, SIZE, 1, 777);
+	cout << "\nPrint array with Int's after editing:" << endl;
+	print(arrayInt, SIZE);
+
+#endif // INTEGER
+
+#ifdef DOUBLE
+
+	double arrayDouble[SIZE];
+
+	cout << "Fill and print array with Double's:" << endl;
+	fill(arrayDouble, SIZE);
+	print(arrayDouble, SIZE);
+	cout << "\nMin element = " << searchMin(arrayDouble, SIZE) << endl;
+	cout << "Max element = " << searchMax(arrayDouble, SIZE) << "\n\n";
+	cout << "Print array with Double's after sorting:" << endl;
+	sort(arrayDouble, SIZE);
+	print(arrayDouble, SIZE);
+	editEl(arrayDouble, SIZE, 5, 77.77);
+	cout << "\nPrint array with Double's after editing:" << endl;
+	print(arrayDouble, SIZE);
+
+#endif // DOUBLE
+
+#ifdef CHAR
+
+	char arrayChar[SIZE];
+
+	cout << "Fill and print array with Char's:" << endl;
+	fill(arrayChar, SIZE);
+	print(arrayChar, SIZE);
+	cout << "\nMin element = " << searchMin(arrayChar, SIZE) << endl;
+	cout << "Max element = " << searchMax(arrayChar, SIZE) << "\n\n";
+	cout << "Print array with Char's after sorting:" << endl;
+	sort(arrayChar, SIZE);
+	print(arrayChar, SIZE);
+	editEl(arrayChar, SIZE, 1, 'Z');
+	cout << "\nPrint array with Char's after editing:" << endl;
+	print(arrayChar, SIZE);
+
+#endif // CHAR
+
+}
---
+++ ps/week13/task2/chernykh/homeWork13Task2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define SQUARE(a) ((a) * (a))
+#define DEGREE_A_IN_B(a, b) (pow(a, b))
+#define IS_EVEN_NUMBER(a) (a % 2 == 0 ? "Even" : "Not even")
+#define IS_NOT_EVEN_NUMBER(a) (a % 2 == 1 ? "Not even" : "Even")
+
+using namespace std;
+
+int main()
+{
+    double firstNumber = 1.90;
+    double secondNumber = 1.95;
+    int thirdNumber = 5;
+    int fourthNumber = 4;
+    cout << "Min: " << MIN(firstNumber, secondNumber) << endl;
+    cout << "Max: " << MAX(firstNumber, secondNumber) << endl;
+    cout << "Square: " << SQUARE(firstNumber) << endl;
+    cout << "Degree: " << DEGREE_A_IN_B(thirdNumber, fourthNumber) << endl;
+    cout << "Number: " << thirdNumber << " " << IS_EVEN_NUMBER(thirdNumber) << endl;
+    cout << "Number: " << fourthNumber << " " << IS_NOT_EVEN_NUMBER(fourthNumber) << endl;
+}
