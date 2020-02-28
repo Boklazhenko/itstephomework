acfc7da2fb82e623098280ab9db845bce374122c
Stepan-K <exotic321@mail.ru>
2020-02-28 21:20:22 +0500

    home work week9

---
+++ ps/week9/task1/kiryakov/task1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	setlocale(LC_ALL, "");
+
+	FILE* test;
+	fopen_s(&test, "test.txt", "a+");
+
+	for (int i = 1; i < argc; ++i) {
+		fprintf(test, "%s\n", argv[i]);
+	}
+	fclose(test);
+
+	if (argc == 1) {
+
+		fopen_s(&test, "test.txt", "r");
+		char buff[255];
+		while (fscanf_s(test, "%s\n", buff, _countof(buff)) != -1) {
+
+			cout << buff << std::endl;
+		}
+		fclose(test);
+	}
+
+
+	system("pause");
+	return 0;
+}
